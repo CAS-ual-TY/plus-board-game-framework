bf46fd22d9007cf78a22325641170c750282d7aa
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2021-11-10 14:34:24 +0100

    OptionsScreen als Beispiel implementiert

--- src/main/java/sweng_plus/boardgames/ludo/gui/OptionsScreen.java
+++ src/main/java/sweng_plus/boardgames/ludo/gui/OptionsScreen.java
@@ -1,15 +1,62 @@
 package sweng_plus.boardgames.ludo.gui;
 
 import org.lwjgl.glfw.GLFW;
-import sweng_plus.framework.userinterface.gui.IScreenHolder;
+import sweng_plus.framework.userinterface.gui.AnchorPoint;
 import sweng_plus.framework.userinterface.gui.Screen;
 import sweng_plus.framework.userinterface.gui.WrappedScreen;
+import sweng_plus.framework.userinterface.gui.util.Texture;
+import sweng_plus.framework.userinterface.gui.util.TextureHelper;
+import sweng_plus.framework.userinterface.gui.widget.ButtonWidget;
+import sweng_plus.framework.userinterface.gui.widget.Dimensions;
+
+import java.io.IOException;
 
 public class OptionsScreen extends WrappedScreen
 {
     public OptionsScreen(Screen subScreen)
     {
         super(subScreen);
+        
+        Dimensions topButton = new Dimensions(400, 100, AnchorPoint.M, 0, -150);
+        Dimensions middleButton = new Dimensions(400, 100, AnchorPoint.M, 0, 0);
+        Dimensions bottomButton = new Dimensions(400, 100, AnchorPoint.M, 0, 150);
+        
+        try
+        {
+            Texture buttonActive = TextureHelper.createTexture("src/main/resources/textures/button_test_active.png");
+            Texture buttonInactive = TextureHelper.createTexture("src/main/resources/textures/button_test_inactive.png");
+            
+            widgets.add(new ButtonWidget(this, topButton, buttonActive, buttonInactive)
+            {
+                @Override
+                protected void clicked(int mouseX, int mouseY, int mods)
+                {
+                
+                }
+            });
+            
+            widgets.add(new ButtonWidget(this, middleButton, buttonActive, buttonInactive)
+            {
+                @Override
+                protected void clicked(int mouseX, int mouseY, int mods)
+                {
+                
+                }
+            });
+            
+            widgets.add(new ButtonWidget(this, bottomButton, buttonActive, buttonInactive)
+            {
+                @Override
+                protected void clicked(int mouseX, int mouseY, int mods)
+                {
+                
+                }
+            });
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
     }
     
     @Override
