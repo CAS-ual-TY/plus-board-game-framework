f3daa50fefba3cf4cbfa0a505cfd7751e3dbd3c6
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2021-11-10 12:33:53 +0100

    WrappedScreen hinzugefügt

---
+++ src/main/java/sweng_plus/framework/userinterface/gui/WrappedScreen.java
@@ -0,0 +1,41 @@
+package sweng_plus.framework.userinterface.gui;
+
+import sweng_plus.framework.userinterface.gui.util.Color4f;
+
+import static org.lwjgl.opengl.GL11.*;
+
+public class WrappedScreen extends Screen
+{
+    public Screen subScreen;
+    
+    public WrappedScreen(IScreenHolder screenHolder, Screen subScreen)
+    {
+        super(screenHolder);
+        this.subScreen = subScreen;
+    }
+    
+    public void returnToSubScreen()
+    {
+        screenHolder.setScreen(subScreen);
+    }
+    
+    @Override
+    public void render(float deltaTick, int mouseX, int mouseY)
+    {
+        renderBackground(deltaTick);
+        super.render(deltaTick, mouseX, mouseY);
+    }
+    
+    public void renderBackground(float deltaTick)
+    {
+        subScreen.render(deltaTick, -1, -1);
+        
+        Color4f.BACKGROUND.glColor4f();
+        glBegin(GL_QUADS);
+        glVertex3f(0, 0, 0); // Oben Links
+        glVertex3f(0, screenH, 0); // Unten Links
+        glVertex3f(screenW, screenH, 0); // Unten Rechts
+        glVertex3f(screenW, 0, 0); // Oben Rechts
+        glEnd();
+    }
+}
