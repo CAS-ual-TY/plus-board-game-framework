9793bc07b2cd0bf0b738f082f443eba821ec1e8e
Julia Eder <julia.eder@stud.sbg.ac.at>
2021-11-13 12:29:36 +0100

    TextWidget und FunctionalTextWidget verbessert

--- src/main/java/sweng_plus/framework/userinterface/gui/widget/FunctionalTextWidget.java
+++ src/main/java/sweng_plus/framework/userinterface/gui/widget/FunctionalTextWidget.java
@@ -6,14 +6,18 @@ import sweng_plus.framework.userinterface.gui.util.Color4f;
 import java.util.List;
 import java.util.function.Supplier;
 
-public class FunctionalTextWidget extends TextWidget
+public class FunctionalTextWidget extends Widget
 {
+    protected FontRenderer renderer;
     protected Supplier<List<String>> text;
+    protected Color4f color;
     
     public FunctionalTextWidget(IWidgetParent parent, Dimensions dimensions, FontRenderer renderer, Supplier<List<String>> text, Color4f color)
     {
-        super(parent, dimensions, renderer, null, color);
+        super(parent, dimensions);
+        this.renderer = renderer;
         this.text = text;
+        this.color = color;
     }
     
     public FunctionalTextWidget(IWidgetParent parent, Dimensions dimensions, FontRenderer renderer, Supplier<List<String>> text)
@@ -22,7 +26,23 @@ public class FunctionalTextWidget extends TextWidget
         this.text = text;
     }
     
+    public FunctionalTextWidget adjustSizeToText()
+    {
+        dimensions.w = renderer.getTextWidth(getText());
+        dimensions.h = renderer.getTextHeight(getText());
+        
+        return this;
+    }
+    
     @Override
+    public void render(float deltaTick, int mouseX, int mouseY)
+    {
+        super.render(deltaTick, mouseX, mouseY);
+        
+        color.glColor4f();
+        renderer.renderCentered(dimensions.x + dimensions.w / 2, dimensions.y + dimensions.h / 2, getText());
+    }
+    
     public List<String> getText()
     {
         return text.get();
--- src/main/java/sweng_plus/framework/userinterface/gui/widget/TextWidget.java
+++ src/main/java/sweng_plus/framework/userinterface/gui/widget/TextWidget.java
@@ -5,44 +5,15 @@ import sweng_plus.framework.userinterface.gui.util.Color4f;
 
 import java.util.List;
 
-public class TextWidget extends Widget
+public class TextWidget extends FunctionalTextWidget
 {
-    protected FontRenderer renderer;
-    protected List<String> text;
-    protected Color4f color;
-    
     public TextWidget(IWidgetParent parent, Dimensions dimensions, FontRenderer renderer, List<String> text, Color4f color)
     {
-        super(parent, dimensions);
-        this.renderer = renderer;
-        this.text = text;
-        this.color = color;
+        super(parent, dimensions, renderer, () -> text, color);
     }
     
     public TextWidget(IWidgetParent parent, Dimensions dimensions, FontRenderer renderer, List<String> text)
     {
         this(parent, dimensions, renderer, text, Color4f.NEUTRAL);
-    }
-    
-    public TextWidget adjustSizeToText()
-    {
-        dimensions.w = renderer.getTextWidth(getText());
-        dimensions.h = renderer.getTextHeight(getText());
-        
-        return this;
-    }
-    
-    @Override
-    public void render(float deltaTick, int mouseX, int mouseY)
-    {
-        super.render(deltaTick, mouseX, mouseY);
-        
-        color.glColor4f();
-        renderer.renderCentered(dimensions.x + dimensions.w / 2, dimensions.y + dimensions.h / 2, getText());
-    }
-    
-    public List<String> getText()
-    {
-        return text;
     }
 }
