d17c2f7c3cf55029c96156aec6d9c5c02b360aa4
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2021-11-29 09:16:18 +0100

    Update LudoTextures.java

--- src/main/java/sweng_plus/boardgames/ludo/gui/LudoTextures.java
+++ src/main/java/sweng_plus/boardgames/ludo/gui/LudoTextures.java
@@ -9,11 +9,12 @@ public class LudoTextures
 {
     public static Texture activeButton;
     public static Texture inactiveButton;
-    
+    public static Texture node;
     
     public static void load() throws IOException
     {
         activeButton = TextureHelper.createTexture("src/test/resources/textures/button_test_active.png");
         inactiveButton = TextureHelper.createTexture("src/test/resources/textures/button_test_inactive.png");
+        node = TextureHelper.createTexture("src/main/resources/textures/node.png");
     }
 }
