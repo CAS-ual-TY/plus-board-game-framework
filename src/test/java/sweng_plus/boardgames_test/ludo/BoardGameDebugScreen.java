f2174efe76166e625c0069383851fea6a8fe356a
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2021-11-29 09:16:06 +0100

    Update BoardGameDebugScreen.java

--- src/test/java/sweng_plus/boardgames_test/ludo/BoardGameDebugScreen.java
+++ src/test/java/sweng_plus/boardgames_test/ludo/BoardGameDebugScreen.java
@@ -2,23 +2,26 @@ package sweng_plus.boardgames_test.ludo;
 
 import sweng_plus.boardgames.ludo.gamelogic.LudoBoard;
 import sweng_plus.boardgames.ludo.gui.LudoBoardMapper;
+import sweng_plus.boardgames.ludo.gui.LudoTextures;
+import sweng_plus.boardgames.ludo.gui.widget.LudoNodeWidget;
 import sweng_plus.framework.boardgame.nodes_board.TeamColor;
+import sweng_plus.framework.boardgame.nodes_board.interfaces.INode;
 import sweng_plus.framework.userinterface.gui.IScreenHolder;
 import sweng_plus.framework.userinterface.gui.Screen;
-import sweng_plus.framework.userinterface.gui.texture.Texture;
-import sweng_plus.framework.userinterface.gui.texture.TextureHelper;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class BoardGameDebugScreen extends Screen
 {
+    public HashMap<INode, LudoNodeWidget> nodeWidgetMap;
+    
     public BoardGameDebugScreen(IScreenHolder screenHolder) throws IOException
     {
         super(screenHolder);
         
-        Texture t = TextureHelper.createTexture("src/main/resources/textures/node.png");
-        
         LudoBoard b = new LudoBoard(TeamColor.TEAMS_6);
-        widgets.addAll(LudoBoardMapper.mapLudoBoard(this, b, t).values());
+        nodeWidgetMap = LudoBoardMapper.mapLudoBoard(this, b, LudoTextures.node);
+        widgets.addAll(nodeWidgetMap.values());
     }
 }
