9e321f4648ba208e33ce2c8022308e573ef8e40c
Julia Eder <julia.eder@stud.sbg.ac.at>
2021-11-30 16:42:32 +0100

    würfel-screen implementiert

---
+++ src/main/java/sweng_plus/boardgames/ludo/gamelogic/networking/RollMessage.java
@@ -0,0 +1,10 @@
+package sweng_plus.boardgames.ludo.gamelogic.networking;
+
+import sweng_plus.framework.networking.util.CircularBuffer;
+
+import java.util.Optional;
+
+public class RollMessage
+{
+
+}
