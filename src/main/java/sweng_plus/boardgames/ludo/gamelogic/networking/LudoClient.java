435c4e78a3e2a89b87cd80536dd7556d529a0672
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2021-11-30 10:07:11 +0100

    Create LudoClient.java

---
+++ src/main/java/sweng_plus/boardgames/ludo/gamelogic/networking/LudoClient.java
@@ -0,0 +1,31 @@
+package sweng_plus.boardgames.ludo.gamelogic.networking;
+
+import sweng_plus.framework.networking.Client;
+import sweng_plus.framework.networking.util.ClientStatus;
+import sweng_plus.framework.networking.util.NetworkRole;
+
+public class LudoClient extends Client
+{
+    public int teamIndex;
+    
+    protected LudoClient(String ip, NetworkRole role, ClientStatus status)
+    {
+        super(ip, role, status);
+        teamIndex = -1;
+    }
+    
+    public void setTeamIndex(int teamIndex)
+    {
+        this.teamIndex = teamIndex;
+    }
+    
+    public int getTeamIndex()
+    {
+        return teamIndex;
+    }
+    
+    public boolean isPlayer()
+    {
+        return teamIndex != -1;
+    }
+}
