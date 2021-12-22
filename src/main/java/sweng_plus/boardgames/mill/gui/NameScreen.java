3ae18a3aba06e3278230b8196bcae7ec3190a2a4
Julia Eder <julia.eder@stud.sbg.ac.at>
2021-12-22 18:58:57 +0100

    Create NameScreen.java

---
+++ src/main/java/sweng_plus/boardgames/mill/gui/NameScreen.java
@@ -0,0 +1,32 @@
+package sweng_plus.boardgames.mill.gui;
+
+import sweng_plus.boardgames.mill.Mill;
+import sweng_plus.boardgames.mill.gui.MenuScreen;
+import sweng_plus.boardgames.mill.gui.util.MillStyles;
+import sweng_plus.framework.userinterface.gui.IScreenHolder;
+import sweng_plus.framework.userinterface.gui.Screen;
+import sweng_plus.framework.userinterface.gui.style.TextStyle;
+import sweng_plus.framework.userinterface.gui.util.AnchorPoint;
+import sweng_plus.framework.userinterface.gui.util.Color4f;
+import sweng_plus.framework.userinterface.gui.widget.FunctionalButtonWidget;
+import sweng_plus.framework.userinterface.gui.widget.SimpleWidget;
+import sweng_plus.framework.userinterface.gui.widget.base.Dimensions;
+
+public class NameScreen extends Screen
+{
+    public NameScreen(IScreenHolder screenHolder)
+    {
+        super(screenHolder);
+        
+        Dimensions textDims = new Dimensions(800, 0, AnchorPoint.M, 0, 0);
+        widgets.add(new SimpleWidget(screenHolder, textDims, new TextStyle(Mill.instance().fontRenderer64, Mill.instance().names, Color4f.BLACK)));
+        
+        Dimensions cancelDims = new Dimensions(350, 80, AnchorPoint.L, 100, 400);
+        widgets.add(new FunctionalButtonWidget(screenHolder, cancelDims, MillStyles.makeButtonStyle("Zurück zum Menü"), this::cancel));
+    }
+    
+    private void cancel()
+    {
+        screenHolder.setScreen(new MenuScreen(screenHolder));
+    }
+}
