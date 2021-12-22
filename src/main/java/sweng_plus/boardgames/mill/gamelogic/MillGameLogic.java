2434d7454ebd89e372adde9c9fecd7f05d48ad9f
FabianSchwaiger <fabian.schwaiger@aon.at>
2021-12-22 18:37:10 +0100

    MillBoard-Verweis und Win-Condition implementiert

--- src/main/java/sweng_plus/boardgames/mill/gamelogic/MillGameLogic.java
+++ src/main/java/sweng_plus/boardgames/mill/gamelogic/MillGameLogic.java
@@ -7,6 +7,9 @@ public class MillGameLogic
     private TeamColor[] teams;
     private final boolean isHost;
     
+    private MillBoard millBoard;
+    private int currentTeamIndex;
+    
     public MillGameLogic(TeamColor[] teams, boolean isHost) {
         this.teams = teams;
         this.isHost = isHost;
@@ -15,4 +18,35 @@ public class MillGameLogic
     public void startGame()
     {
     }
+    
+    public TeamColor gameWon() {
+        for(TeamColor team : teams)
+        {
+            if(millBoard.getTeamFigures(team).length < 3) {
+                return team;
+            }
+        }
+        return null;
+    }
+    
+    public boolean isGameWon() {
+        return gameWon() != null;
+    }
+    
+    public TeamColor[] getTeams()
+    {
+        return teams;
+    }
+    
+    public MillBoard getMillBoard()
+    {
+        return millBoard;
+    }
+    
+    // TODO Figur auswählen
+        // TODO Aufbau oder Zugphase
+    // TODO Zielfeld auswählen
+    // TODO auf Mühle prüfen
+    // TODO gegnerische Figur zum Entfernen auswählen
+        // TODO Wincondition
 }
