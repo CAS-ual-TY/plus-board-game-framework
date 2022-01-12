0b7fdeac80f5ca8adbd642eed024356d1ca134e8
Julia Eder <julia.eder@stud.sbg.ac.at>
2022-01-12 11:57:09 +0100

    WinScreen verändert

--- src/main/java/sweng_plus/boardgames/mill/gui/WinScreen.java
+++ src/main/java/sweng_plus/boardgames/mill/gui/WinScreen.java
@@ -1,6 +1,7 @@
 package sweng_plus.boardgames.mill.gui;
 
 import sweng_plus.boardgames.mill.Mill;
+import sweng_plus.framework.userinterface.gui.style.ColoredQuadStyle;
 import sweng_plus.framework.userinterface.gui.style.TextStyle;
 import sweng_plus.framework.userinterface.gui.util.AnchorPoint;
 import sweng_plus.framework.userinterface.gui.util.Color4f;
@@ -13,7 +14,9 @@ public class WinScreen extends MillExtensionScreen
     {
         super(subScreen);
         
+        Dimensions quadDims = new Dimensions(4000, 200, AnchorPoint.M, 0, 0);
+        widgets.add(new SimpleWidget(screenHolder, quadDims, new ColoredQuadStyle(new Color4f(232, 225, 218))));
         Dimensions nameTextDims = new Dimensions(800, 0, AnchorPoint.M, 0, 0);
-        widgets.add(new SimpleWidget(screenHolder, nameTextDims, new TextStyle(Mill.instance().fontRenderer64, winner + " hat Gewonnen!", Color4f.WHITE)));
+        widgets.add(new SimpleWidget(screenHolder, nameTextDims, new TextStyle(Mill.instance().fontRenderer64, winner + " hat Gewonnen!", Color4f.BLACK)));
     }
 }
