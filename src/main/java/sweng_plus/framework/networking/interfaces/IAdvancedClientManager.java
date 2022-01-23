d91ee7262c9de884814f2b8a4ad1347cd5f8bf42
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2022-01-23 22:29:34 +0100

    IAdvancedClientManager docs

--- src/main/java/sweng_plus/framework/networking/interfaces/IAdvancedClientManager.java
+++ src/main/java/sweng_plus/framework/networking/interfaces/IAdvancedClientManager.java
@@ -4,9 +4,20 @@ import java.util.UUID;
 
 public interface IAdvancedClientManager extends IClientManager
 {
+    /**
+     * @return The name of this client.
+     */
     String getName();
     
+    /**
+     * Sets the session identifier received from the server (authentication).
+     *
+     * @param sessionIdentifier The session identifier.
+     */
     void setSessionIdentifier(UUID sessionIdentifier);
     
+    /**
+     * @return The client identifier based on the session identifier set in {@link #setSessionIdentifier(UUID)}.
+     */
     UUID getClientIdentifierForSession();
 }
