2c6c04faee028b3dab76384907521b9b0e728f7c
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2022-01-23 22:57:20 +0100

    IAdvancedHostManager docs

--- src/main/java/sweng_plus/framework/networking/interfaces/IAdvancedHostManager.java
+++ src/main/java/sweng_plus/framework/networking/interfaces/IAdvancedHostManager.java
@@ -4,19 +4,42 @@ import java.util.UUID;
 
 public interface IAdvancedHostManager<C extends IAdvancedClient> extends IHostManager<C>, IAdvancedClientManager
 {
+    /**
+     * Orderly kicks a client from the server (the client is notified about the kick)
+     * and closes the connection to the client server-side.
+     *
+     * @param client The {@link IAdvancedClient} to kick.
+     */
     void kickClient(C client);
     
+    /**
+     * Orderly kicks a client from the server (the client is notified about the kick with a given message)
+     * and closes the connection to the client server-side.
+     *
+     * @param client  The {@link IAdvancedClient} to kick.
+     * @param message The message which can represent the kick reason or kick justification.
+     */
     default void kickClient(C client, String message)
     {
         kickClient(client);
     }
     
+    /**
+     * Called when the client answers the authentication request of the server.
+     *
+     * @param client     The {@link IAdvancedClient} which authenticated.
+     * @param name       The name of the client.
+     * @param identifier The identifier of the client.
+     */
     default void authenticate(C client, String name, UUID identifier)
     {
         client.setName(name.trim());
         client.setUUID(identifier);
     }
     
+    /**
+     * @return The server's session identifier which is sent to the clients for server/session authentication.
+     */
     UUID getSessionIdentifier();
     
     @Override
