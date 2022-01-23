0ef64ede6bec6a69989fc024b87b13f83256cef0
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2022-01-24 00:35:01 +0100

    IHostEventsListener docs

--- src/main/java/sweng_plus/framework/networking/interfaces/IHostEventsListener.java
+++ src/main/java/sweng_plus/framework/networking/interfaces/IHostEventsListener.java
@@ -2,31 +2,43 @@ package sweng_plus.framework.networking.interfaces;
 
 public interface IHostEventsListener<C extends IClient>
 {
-    void clientConnected(C client); // Called from main thread
+    /**
+     * Called after a client has successfully connected to the server.
+     *
+     * @param client The {@link C} instance representing the client which has connected to the server.
+     */
+    void clientConnected(C client);
     
-    default void clientSocketClosed(C client) // Called from main thread
+    /**
+     * Called after a client connection socket has closed without an exception thrown.
+     *
+     * @param client The {@link C} instance representing the client the socket has closed of.
+     */
+    default void clientSocketClosed(C client)
     {
     
     }
     
-    default void clientSocketClosedWithException(C client, Exception e) // Called from main thread
+    /**
+     * Called after a client connection socket has closed without an exception thrown.
+     *
+     * @param client The {@link C} instance representing the client the socket has closed of.
+     * @param e      The thrown exception.
+     */
+    default void clientSocketClosedWithException(C client, Exception e)
     {
         clientSocketClosed(client);
     }
     
+    /**
+     * @return A dummy {@link IHostEventsListener} doing nothing.
+     */
     static <C extends IClient> IHostEventsListener<C> emptyHostListener()
     {
         return new IHostEventsListener<>()
         {
-            
             @Override
             public void clientConnected(C client)
-            {
-            
-            }
-            
-            @Override
-            public void clientSocketClosed(C client)
             {
             
             }
