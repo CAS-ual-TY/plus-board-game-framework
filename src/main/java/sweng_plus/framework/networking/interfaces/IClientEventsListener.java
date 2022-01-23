fbbc72358815f34d83ef7b330d855f8932c28bd6
CAS-ual-TY <41904979+CAS-ual-TY@users.noreply.github.com>
2022-01-23 23:21:11 +0100

    IClientEventsListener docs

--- src/main/java/sweng_plus/framework/networking/interfaces/IClientEventsListener.java
+++ src/main/java/sweng_plus/framework/networking/interfaces/IClientEventsListener.java
@@ -2,31 +2,32 @@ package sweng_plus.framework.networking.interfaces;
 
 public interface IClientEventsListener
 {
+    /**
+     * Called after the connection socket has closed orderly without an exception thrown.
+     */
     default void socketClosed() // Called from main thread
     {
     
     }
     
+    /**
+     * Called after the connection socket has closed with an exception thrown.
+     *
+     * @param e The thrown exception.
+     */
     default void socketClosedWithException(Exception e) // Called from main thread
     {
         socketClosed();
     }
     
-    static IClientEventsListener emptyClientListener()
+    /**
+     * @return A dummy {@link IClientEventsListener} doing nothing.
+     */
+    static IClientEventsListener emptyClientEventsListener()
     {
         return new IClientEventsListener()
         {
-            @Override
-            public void socketClosed()
-            {
-            
-            }
-            
-            @Override
-            public void socketClosedWithException(Exception e)
-            {
-            
-            }
+        
         };
     }
 }
